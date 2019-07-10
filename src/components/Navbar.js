6db8664d404b8c1e37f9e26c29a925275cc4f66a
Milan <milan.dhar@gmail.com>
2019-07-10 16:48:50 -0700

    added routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react' 
+import { Menu } from 'semantic-ui-react'
 import logo from '../images/logo.png'
+import { Link } from 'react-router-dom'
 
 export default class Navbar extends Component {
 
 //state here
     render() {
-        return( 
+        return(
             <div>
                <div class="ui top attached menu">
                 <div class="item">
+
+                <a class="item" href="http://localhost:3001/browse">
                 <img class="ui avatar image" src={logo} />
                 Browse
+                </a>
                 </div>
-                
-                <a class="item">
+
+
+                <a href="http://localhost:3001/liked" class="item">
                 Favorites
                 </a>
                 <div class="right menu">
-                <a class="item">
+                <a href="http://localhost:3001/profile" class="item">
                 Profile
                 </a>
                 <div class="item">
-                <div class="ui secondary button">Log Out</div>
+                <div href="http://localhost:3001/" class="ui secondary button">Log Out</div>
             </div>
             </div>
             </div>
-           
-          
+
+
             </div>
         )
     }
