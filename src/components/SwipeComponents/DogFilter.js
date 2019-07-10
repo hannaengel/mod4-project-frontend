709379b500560ebd0d41e68e0a5f11efe9a79a15
Milan <milan.dhar@gmail.com>
2019-07-10 14:14:37 -0700

    first draft of frontend form

--- src/components/SwipeComponents/DogFilter.js
+++ src/components/SwipeComponents/DogFilter.js
@@ -5,51 +5,79 @@ export default class DogFilter extends Component {
   constructor(){
     super()
     this.state = {
-      desiredSex: ['all'],
-      desiredAge: ['all']
+      desiredSex: ['all-sexes'],
+      desiredAge: ['all-ages']
     }
   }
 
   componentDidMount(){
-    if (this.state.desiredSex[0] === 'all') {
+    if (this.state.desiredSex[0] === 'all-sexes') {
       let allBtn = document.getElementById("all-ages")
       allBtn.classList.add("active")
     }
-    if (this.state.desiredAge[0] === 'all') {
+    if (this.state.desiredAge[0] === 'all-ages') {
       let allBtn = document.getElementById("all-sexes")
       allBtn.classList.add("active")
     }
-    this.state.desiredAge 
+    this.state.desiredAge.forEach((age) => {
+      this.activateBtn(age)
+    })
+    this.state.desiredSex.forEach((sex) => {
+      this.activateBtn(sex)
+    })
+  }
+
+  handleSexClick = (ev) => {
+    ev.persist()
+    console.log(ev)
+    let btnName = ev.currentTarget.attributes[0].value
+    let selectedBtn = document.getElementById(btnName)
+    let allBtn = document.getElementById("all-sexes")
+    if (this.state.desiredSex[0] === 'all-sexes') {
+      allBtn.classList.remove("active")
+      const {desiredSex, ...state} = this.state
+      this.setState({state})
+      this.setState({desiredSex: [btnName]})
+      this.activateBtn(btnName)
+    }
   }
 
   handleAgeClick = (ev) => {
-    ev.preventDefault()
     ev.persist()
     let btnName = ev.currentTarget.attributes[0].value
     let selectedBtn = document.getElementById(btnName)
     let allBtn = document.getElementById("all-ages")
-    if (this.state.desiredSex[0] === 'all') {
+    if (this.state.desiredAge[0] === 'all-ages') {
+      console.log(allBtn)
+      console.log('in if')
       allBtn.classList.remove("active")
-      const {desiredSex, ...state} = this.state
+      const {desiredAge, ...state} = this.state
       this.setState({state})
       this.setState({desiredAge: [btnName]})
+      this.activateBtn(btnName)
     }
   }
+
+  activateBtn = (btnName) => {
+    let selectedBtn = document.getElementById(btnName)
+    selectedBtn.classList.add("active")
+  }
+
 
   render(){
     return(
     <div class = "dog-form-wrapper">
       <div class = "age-icons">
-        <div id="all-ages" name="all-ages" class="ui right attached button" role="button" tabindex="0">All</div>
-        <div id = "baby" name="baby" onClick={this.handleAgeClick} class="ui right attached button" role="button" tabindex="0">Baby</div>
-        <div id = "young" name="young" class="ui right attached button" role="button" tabindex="0">Young</div>
-        <div id = "adult" name="adult" class="ui right attached button" role="button" tabindex="0">Adult</div>
-        <div id = "senior" name="senior" class="ui right attached button" role="button" tabindex="0">Senior</div>
+        <div onClick={this.handleAgeClick} id="all-ages" name="all-ages" class="ui right attached button" role="button" tabindex="0">All</div>
+        <div onClick={this.handleAgeClick} id = "baby" name="baby" class="ui right attached button" role="button" tabindex="0">Baby</div>
+        <div onClick={this.handleAgeClick} id = "young" name="young" class="ui right attached button" role="button" tabindex="0">Young</div>
+        <div onClick={this.handleAgeClick} id = "adult" name="adult" class="ui right attached button" role="button" tabindex="0">Adult</div>
+        <div onClick={this.handleAgeClick}  id = "senior" name="senior" class="ui right attached button" role="button" tabindex="0">Senior</div>
       </div>
       <div class = "sex-icons">
-        <div id="all-sexes" name="all-sexes" class="ui right attached button" role="button" tabindex="0">All</div>
-        <div id = "female" name="female" class="ui right attached button" role="button" tabindex="0"><i class = "venus icon"></i></div>
-        <div id = "male" name="male" class="ui right attached button" role="button" tabindex="0"><i class = "mars icon"></i></div>
+        <div onClick={this.handleSexClick} id="all-sexes" name="all-sexes" class="ui right attached button" role="button" tabindex="0">All</div>
+        <div onClick={this.handleSexClick} id = "female" name="female" class="ui right attached button" role="button" tabindex="0"><i class = "venus icon"></i></div>
+        <div onClick={this.handleSexClick} id = "male" name="male" class="ui right attached button" role="button" tabindex="0"><i class = "mars icon"></i></div>
       </div>
     </div>
 
